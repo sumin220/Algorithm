1aca1bc24f70e5a20d426fa87dbef099c44c0519
Son Soo Min <sonsumin00@gmail.com>
2024-09-15 16:48:42 +0900

    [Bronze V] Title: 삼각형, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+
+        float a = (W * H) / 2.0f;
+        System.out.println(a);
+    }
+}
