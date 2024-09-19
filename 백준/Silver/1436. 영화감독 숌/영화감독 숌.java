3535cc9a54b97f11d9f9a86a23f52f5a6824d2ea
Son Soo Min <sonsumin00@gmail.com>
2024-09-19 13:43:12 +0900

    [Silver V] Title: 영화감독 숌, Time: 260 ms, Memory: 85984 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 0;  
+        int num = 666;  
+
+        while (true) {
+            if (String.valueOf(num).contains("666")) { 
+                count++;
+            }
+            if (count == N) {  
+                System.out.println(num);
+                break;
+            }
+            num++;  
+        }
+    }
+}
