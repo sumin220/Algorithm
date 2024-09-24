978d1e20c80a84473cbc4e20a23d66679549b437
Son Soo Min <sonsumin00@gmail.com>
2024-09-24 13:37:07 +0900

    [Silver II] Title: 최대 힙, Time: 884 ms, Memory: 34072 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        //최대힙 구현
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input != 0) {
+                queue.add(input);
+            } else {
+                queue.add(0);
+                System.out.println(queue.peek());
+                queue.remove(queue.peek());
+            }
+        }
+    }
+}
