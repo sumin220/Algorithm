e8c8cefc3fa05f8f776aa83185979fcc016b9179
Son Soo Min <sonsumin00@gmail.com>
2024-10-02 13:39:54 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 608 ms, Memory: 294836 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        int N = Integer.parseInt(st.nextToken()); //7
+        int K = Integer.parseInt(st.nextToken()); //3
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        while (!queue.isEmpty()) {
+
+            for (int i = 0; i < K - 1; i++) {
+                queue.add(queue.poll());
+            }
+            sb.append(queue.poll());
+            if (!queue.isEmpty()) {
+                sb.append(", ");
+            }
+        }
+        sb.append(">");
+        System.out.println(sb.toString());
+
+    }
+}
