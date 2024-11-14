bae7d7f4584401d33ca450b0b59eeeb970259471
Soomin <sonsumin00@gmail.com>
2024-11-14 23:41:09 +0900

    [Bronze V] Title: N 찍기, Time: 748 ms, Memory: 30224 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
