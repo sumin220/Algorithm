718181cd730b27a1bd76800458fabe3b7a535c77
Soomin <sonsumin00@gmail.com>
2024-11-26 10:13:42 +0900

    [Gold V] Title: 동전, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            int[] coins = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                coins[i] = Integer.parseInt(st.nextToken());
+            }
+            int M = Integer.parseInt(br.readLine());
+            int[] dp = new int[M + 1];
+            dp[0] = 1;
+
+            for (int coin : coins) {
+                for (int j = coin; j <= M; j++) {
+                    dp[j] += dp[j - coin];
+                }
+            }
+
+            sb.append(dp[M]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
