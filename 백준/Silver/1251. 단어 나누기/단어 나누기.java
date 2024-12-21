9b8d0873fd7c654df50b279e9bc96f898ad83189
Soomin <sonsumin00@gmail.com>
2024-12-21 21:03:15 +0900

    [Silver V] Title: 단어 나누기, Time: 144 ms, Memory: 17068 KB -BaekjoonHub

---
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        String result = null;
+
+        for (int i = 1; i < word.length() - 1; i++) {
+            for (int j = i + 1; j < word.length(); j++) {
+                String part1 = new StringBuilder(word.substring(0, i)).reverse().toString();
+                String part2 = new StringBuilder(word.substring(i, j)).reverse().toString();
+                String part3 = new StringBuilder(word.substring(j)).reverse().toString();
+                
+                String combined = part1 + part2 + part3;
+
+                if (result == null || combined.compareTo(result) < 0) {
+                    result = combined;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
