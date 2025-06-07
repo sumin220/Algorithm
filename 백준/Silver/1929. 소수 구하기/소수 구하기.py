cdb33838b69718406117b9a0a924619201870fff
Soomin <sonsumin00@gmail.com>
2025-06-07 23:54:45 +0900

    [Silver III] Title: 소수 구하기, Time: 272 ms, Memory: 44752 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,3 +1,4 @@
+import math
 import sys
 
 input = sys.stdin.readline
@@ -5,11 +6,15 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 
 
-for i in range(N, M+1):
-    if i == 1:
-        continue
-    for j in range(2, int(i**0.5)+1):
-        if i % j == 0:
-            break
-    else:
-        print(i)
+is_prime=[True] * (M+1)
+is_prime[0] = is_prime[1] = False
+
+for i in range(2, int(math.sqrt(M))+1):
+    if is_prime[i]:
+        for j in range(i*i, M+1, i):
+            is_prime[j]=False
+
+prime_list = [i for i in range(N, len(is_prime)) if is_prime[i]]
+
+for i in prime_list:
+    print(i)
