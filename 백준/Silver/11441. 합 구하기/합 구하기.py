ad787d537bb7e45a8c013f7c9b18aa3a825bfa80
Soomin <sonsumin00@gmail.com>
2025-06-13 09:34:48 +0900

    [Silver III] Title: 합 구하기, Time: 216 ms, Memory: 42952 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.py
+++ 백준/Silver/11441. 합 구하기/합 구하기.py
@@ -2,15 +2,15 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+arr = [0] * (N+1)
 arr = list(map(int, input().split()))
 
-prefix_sum = [0] * (N + 1)
+M= int(input())
+
+prefix_sum = [0]*(N+1)
 for i in range(N):
-    prefix_sum[i + 1] = prefix_sum[i] + arr[i]
-
-M = int(input())
-
-for i in range(M):
+    prefix_sum[i+1] = prefix_sum[i]+arr[i]
+for j in range(M):
     a, b = map(int, input().split())
-    result = prefix_sum[b] - prefix_sum[a - 1]
-    print(result)
+
+    print(prefix_sum[b] - prefix_sum[a-1])
